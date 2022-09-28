b7bfe3a899f703644410998e18d85e02912c3fb6
elena tarasova <nafanichka.ev@gmail.com>
2022-09-28 16:10:09 -0400

    Exersize2

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Write a number A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Sum of digits in number {A} is {GetSum(A)}");
+
+int GetSum(int n)
+{
+    int sum = 0;
+    while (n>0)
+    {
+        sum = sum + n%10;
+        n/=10;
+    }
+    return sum;
+}
